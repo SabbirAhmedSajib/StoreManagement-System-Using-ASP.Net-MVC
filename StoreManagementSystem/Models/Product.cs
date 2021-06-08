3412e2fdbc11a9bf54679b6adee246512d420f7d
Sabbir-4557 <78074651+Sabbir-4557@users.noreply.github.com>
2021-06-08 10:18:09 +0600

    ASP.Net MVC Project

---
+++ StoreManagementSystem/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreManagementSystem.Models
+{
+    public class Product
+    {   
+        
+        [Key]
+        public int ProductId { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime ExpiredDate { get; set; }
+    }
+}
